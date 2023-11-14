d21d03acb0f23461363090489833cd967ddbf32e
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-11-14 16:40:11 +0100

    solved all tests

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,10 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
-
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+  switch (typeof stringArrayOrObject) {
+    case 'string':
+      return stringArrayOrObject === ''
+    case 'array':
+      return stringArrayOrObject.length === 0
+    case 'object':
+      return Object.keys(stringArrayOrObject).length === 0
   }
-
-  return false
 }
--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,3 @@
 export const hackerSpeak = (words) => {
-  return words
+  return words.replaceAll("a", "4").replaceAll("e", "3").replaceAll("i", "1").replaceAll("o", "0").replaceAll("s", "5").replaceAll("A", "4").replaceAll("E", "3").replaceAll("I", "1").replaceAll("O", "0").replaceAll("S", "5")
 }
